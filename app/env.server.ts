d7f958fe39319894f5456bba26d5999d0cb2e82b
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-03-12 23:20:26 -0400

    env updates

--- app/env.server.ts
+++ app/env.server.ts
@@ -1,7 +1,6 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  VITE_APP_ENVIRONMENT: z.enum(['dev', 'qa', 'staging', 'prod']),
   VITE_APP_MSW_ENABLED: z.coerce.boolean().optional(),
   NODE_ENV: z.enum(['development', 'test', 'production']),
 });
