57bbab72db0f7f19408937e1126a3516ccb589ee
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-03-14 23:39:57 -0400

    more layout fun

--- app/components/app/Navbar.tsx
+++ app/components/app/Navbar.tsx
@@ -4,6 +4,14 @@ import {
   NavigationMenuItem,
   NavigationMenuLink,
 } from '@/components/ui/NavigationMenu';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '../ui/DropdownMenu';
 
 interface Route {
   to: string;
@@ -27,10 +35,23 @@ const Navbar = () => {
           <NavigationMenuLink to="/">{'Sean McQuaid'}</NavigationMenuLink>
         </NavigationMenuItem>
         {routes.map(route => (
-          <NavigationMenuItem key={route.to}>
+          <NavigationMenuItem key={route.to} className="sm:hidden">
             <NavigationMenuLink to={route.to}>{route.label}</NavigationMenuLink>
           </NavigationMenuItem>
         ))}
+        <NavigationMenuItem className="md:hidden">
+          <DropdownMenu>
+            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
   );
